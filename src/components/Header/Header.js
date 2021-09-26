b00868dd0810763010375bde791afe4cb305533b
ujjaldeb <deb.ujjal.ksa@gmail.com>
2021-09-26 10:24:38 +0600

    first commit with Header component and json data creation

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className="header">
+            <h1>Project Management Team</h1>
+            <p>Project management is the application of processes, methods, skills, knowledge and experience to achieve specific project objectives according to the project acceptance criteria within agreed parameters.</p>
+            <h2>Total Budget: $1B</h2>
+        </div>
+    );
+};
+
+export default Header;
